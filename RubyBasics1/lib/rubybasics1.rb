d625d2361d50fb1f5ef18269441aeb3694f3a553
Ryan Carrere <rcarrie1@uncc.edu>
2019-09-08 19:21:51 +0000

    RubyBasics1 - Part 1

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -2,11 +2,17 @@
 # Part I
 def sum arr
   # YOUR CODE HERE
+  runningtotal = 0
+  if arr.each do |x|
+    runningtotal = runningtotal + x
+  end
+    return runningtotal
+  end
 end
-
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  
 end
 
 # Part III
