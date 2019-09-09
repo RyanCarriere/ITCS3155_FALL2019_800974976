094792ee8cb875bd6f0e0cc0d11a142909359b3d
Ryan Carrere <rcarrie1@uncc.edu>
2019-09-09 01:07:52 +0000

    RubyBasics1 - Part 3 V2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -19,15 +19,29 @@ end
 # Part III
 def sum_to_n? arr, n
  #YOUR CODE HERE
- hash = Hash.new(0)
-  arr.each do |x|
-    if hash.key? x
+  if (arr.length == 0)
+    return false
+  end
+  if (arr.length == 1)
+    return false
+  end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
       return true
-    else
-      hash[n-x] = x
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
     end
   end
   return false
+end
+
 end
  
 
