ce5f13f2a920f7a83e33010b9d3d6fadfc24f93c
Ryan Carrere <rcarrie1@uncc.edu>
2019-09-09 01:03:50 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -13,10 +13,21 @@ end
 def max_2_sum arr
   # YOUR CODE HERE
   return(arr.sort.last(2).sum) 
-  
+
 end
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ #YOUR CODE HERE
+ hash = Hash.new(0)
+  arr.each do |x|
+    if hash.key? x
+      return true
+    else
+      hash[n-x] = x
+    end
+  end
+  return false
 end
+ 
+
