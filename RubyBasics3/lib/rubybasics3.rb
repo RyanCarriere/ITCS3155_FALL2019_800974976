7db59f63078c19fe7707b2e0c1c37168856029ea
Ryan Carrere <rcarrie1@uncc.edu>
2019-10-04 14:02:04 +0000

    Ruby OO basics - Part1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -2,4 +2,13 @@
 
 class BookInStock
 # YOUR CODE HERE
+attr_reader :isbn
+attr_accessor :price
+attr_writer :isbn 
+
+def initialize(isbn, price)
+raise ArgumentError if isbn.empty? || price <= 0
+@isbn = isbn
+@price = Float(price)
+end
 end
