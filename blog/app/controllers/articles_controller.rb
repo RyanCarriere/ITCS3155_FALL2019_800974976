c7093dc4272ae2d618135dc7340dbdfadac555c9
Ryan Carrere <rcarrie1@uncc.edu>
2019-11-03 19:35:07 +0000

    Add blogger adds an article scenario 2

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,27 +2,29 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    
     def show 
         @article = Article.find(params[:id])
     end
+    
     def new
         @article = Article.new
     end
+    
     def create
         @article = Article.new(article_params)
-        byebug
-    if 
-        byebug
-        @article.save
-        redirect_to 
-        @article 
-    else 
-        render 'new' 
+    
+        if @article.save
+            redirect_to @article 
+        else 
+            render 'new'
+        end
     end
    
     def edit 
         @article = Article.find(params[:id]) 
     end
+    
     def update 
         @article = Article.find(params[:id]) 
         if 
@@ -32,15 +34,15 @@ class ArticlesController < ApplicationController
                 render 'edit' 
         end 
     end
+    
     def destroy  
         @article = Article.find(params[:id]) 
         @article.destroy
         redirect_to articles_path 
     end 
-    
-    private
-    def article_params
-    params.require(:article).permit(:title, :text)
-    end
-    end
 end
+
+private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
     def create 
-        @articles = Article.find(params[:article_id])
-        @comments = @articles.comments.create(comment_params)
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
     private 
-    def comment_params
-        params.require(:comment).permit(:commenter, :body)
-    end
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
 end
